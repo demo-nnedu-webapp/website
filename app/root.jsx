d528a99c3a65a40c5cbbfe79dd283a8a1f909578
Senninseyi <seyi.oyebamiji@gmail.com>
2022-11-15 17:42:47 +0100

    fialed dev setup
    change framework to nextjs

--- app/root.jsx
+++ app/root.jsx
@@ -9,6 +9,7 @@ import {
 import { Layout } from "./components/layout/layout";
 import styles from "./styles/app.css";
 
+
 export const meta = () => ({
   charset: "utf-8",
   title: "NNEDU",
@@ -25,6 +26,10 @@ export function links() {
     {
       rel: "stylesheet",
       href: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
+    },
+    {
+      rel: "stylesheet",
+      href: import(''),
     },
   ];
 }
