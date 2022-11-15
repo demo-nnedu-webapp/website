3f5c9abb5a63dab29ef7c529a88c0b37e45a1393
Senninseyi <seyi.oyebamiji@gmail.com>
2022-11-15 11:56:40 +0100

    style: add tailwind css to project

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,8 +2,8 @@ import { Link } from "@remix-run/react";
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to NNEDU</h1>
+    <div className="bg-blue-500 w-full h-full min-h-screen">
+      <h1 className="bg-red-500 p-4 w-full h-full font-montserrat font-bold text-3xl">Welcome to NNEDU</h1>
       <Link to="/about">About</Link>
     </div>
   );
