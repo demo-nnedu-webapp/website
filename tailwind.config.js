c1ddb65f80d92267de23f9546a6110b735a2879d
Senninseyi <seyi.oyebamiji@gmail.com>
2022-11-16 12:46:09 +0100

    chore: add custom config colors and font to tailwind.config

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  // darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        primary: "#1F1839",
+        secondary: "#DEC918",
+        accent: "#0074A8",
+      },
+      fontFamily: {
+        montserrat: ["montserrat", "sans-serif"],
+        inter: ["inter", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
