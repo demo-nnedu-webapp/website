89e7462c4f3f56816925a78d310710f4166b7913
Senninseyi <seyi.oyebamiji@gmail.com>
2022-11-23 11:06:07 +0100

    fixes

--- pages/[uri].js
+++ pages/[uri].js
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import { client } from "../lib/apollo";
 import { gql } from "@apollo/client";
+import { useRouter } from "next/router";
 
 export default function SlugPage({ post }) {
+  const router = useRouter();
+
   return (
     <div className="bg-accent">
       <Head>
@@ -11,24 +14,28 @@ export default function SlugPage({ post }) {
       </Head>
 
       <div className="flex flex-col items-center h-full justify-center">
-        <div className="w-full px-6 py-10 text-white border-b-8 border-secondary max-w-[1440px] h-full">
-          <div className="w-full flex items-center justify-center">
-            <div className="w-full max-w-[70rem] p-4 flex flex-col gap-4">
-              <h1 className="text-4xl text-white font-bold font-inter tracking-[0.06em] capitalize">
-                {post.title}
-              </h1>
-              <article
-                dangerouslySetInnerHTML={{ __html: post.content }}
-                className="font-medium font-montserrat tracking-[0.06em]"
-              ></article>
-              {/* <p>
+        {router.isFallback ? (
+          <div>Loading...</div>
+        ) : (
+          <div className="w-full px-6 py-10 text-white border-b-8 border-secondary max-w-[1440px] h-full">
+            <div className="w-full flex items-center justify-center">
+              <div className="w-full max-w-[70rem] p-4 flex flex-col gap-4">
+                <h1 className="text-4xl text-white font-bold font-inter tracking-[0.06em] capitalize">
+                  {post.title}
+                </h1>
+                <article
+                  dangerouslySetInnerHTML={{ __html: post.content }}
+                  className="font-medium font-montserrat tracking-[0.06em]"
+                ></article>
+                {/* <p>
             ✍️ &nbsp;&nbsp;
             {`${post.author.node.firstName} ${post.author.node.lastName}`} | 🗓️
             &nbsp;&nbsp;{new Date(post.date).toLocaleDateString()}
           </p> */}
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
@@ -65,6 +72,6 @@ export async function getStaticPaths() {
   const paths = [];
   return {
     paths,
-    fallback: "blocking",
+    fallback: true,
   };
 }
