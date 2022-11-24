ca339d1f330ab1cefcb2f228965fd23f68fd25f9
Senninseyi <seyi.oyebamiji@gmail.com>
2022-11-24 12:00:35 +0100

    chore: add scrollRestoration hook

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,10 @@ import "../styles/globals.css";
 import "antd/dist/antd.min.css";
 import { ApolloProvider } from "@apollo/client/react";
 import { client } from "../lib/apollo";
+import useScrollRestoration from "../components/utils/useScrollRestoration";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
+  useScrollRestoration(router)
   return (
     <>
       <ApolloProvider client={client}>
