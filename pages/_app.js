aa004e41d92c978107293ed56e876a89fe7e1279
Senninseyi <seyi.oyebamiji@gmail.com>
2022-12-05 10:48:48 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,11 @@ import { client } from "../lib/apollo";
 import useScrollRestoration from "../components/utils/useScrollRestoration";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
+import Chatbot from "react-chatbot-kit";
+import config from "../lib/chatbot-config/config";
+import "react-chatbot-kit/build/main.css";
+import ActionProvider from "../lib/chatbot-config/actionProvider";
+import MessageParser from "../lib/chatbot-config/messageParser";
 
 function MyApp({ Component, pageProps, router }) {
   useScrollRestoration(router);
@@ -16,6 +21,14 @@ function MyApp({ Component, pageProps, router }) {
           <Layout>
             <Component {...pageProps} />
           </Layout>
+          //TODO ::: add chatbot feature
+          {/* <div className="fixed p-4 bottom-0 right-0">
+            <Chatbot
+              config={config}
+              actionProvider={ActionProvider}
+              messageParser={MessageParser}
+            />
+          </div> */}
         </ApolloProvider>
       </Provider>
     </>
