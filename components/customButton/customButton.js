27693884cdb3c241030d34798d3ca71edda5edab
Senninseyi <seyi.oyebamiji@gmail.com>
2023-01-28 07:28:14 +0100

    Update customButton.js

--- components/customButton/customButton.js
+++ components/customButton/customButton.js
@@ -1,6 +1,12 @@
 import React from "react";
 
-export const PrimaryButton = ({ children, type, onClick, className }) => {
+export const PrimaryButton = ({
+  children,
+  type,
+  onClick,
+  className,
+  style,
+}) => {
   return (
     <button
       className={`${className} bg-secondary w-auto lg:w-fit px-5 py-3 
@@ -8,6 +14,7 @@ export const PrimaryButton = ({ children, type, onClick, className }) => {
       hover:bg-primary hover:border-secondary hover:border-2 hover:text-white`}
       type={type}
       onClick={onClick}
+      style={style}
     >
       {children}
     </button>
